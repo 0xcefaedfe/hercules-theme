6771d3d815708fbb273c5688fa3b9442f54d1355
T <t@nxtsqr.com>
2025-07-18 22:23:09 +0200

    Add package file

---
+++ hercules-theme-pkg.el
@@ -0,0 +1,7 @@
+(define-package "hercules-theme" "1.0.0"
+  "A minimalist dark theme with amber accents"
+  '((emacs "24.3"))
+  :keywords '("faces" "themes")
+  :authors '(("Tamas Zsar" . "t@nxtsqr.com"))
+  :maintainer '("Tamas Zsar" . "t@nxtsqr.com")
+  :url "https://github.com/0xcefaedfe/hercules-theme")
